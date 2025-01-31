a64132fcaa2e81e081e30eff76e898ab568136b5
Rizky <riskyasyam@gmail.com>
2025-01-31 23:22:17 +0700

    setup laravel and tailwind

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,10 @@
+<nav class="bg-blue-600 p-4 shadow-lg">
+    <div class="container mx-auto flex justify-between items-center">
+        <a href="#" class="text-white text-xl font-semibold">My App</a>
+        <ul class="flex space-x-4">
+            <li><a href="#" class="text-white hover:underline">Home</a></li>
+            <li><a href="#" class="text-white hover:underline">About</a></li>
+            <li><a href="#" class="text-white hover:underline">Contact</a></li>
+        </ul>
+    </div>
+</nav>
